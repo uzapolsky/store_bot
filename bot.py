5b9fc8243277052aa168a28332ed569326195570
uzapolsky <uzapolsky@gmail.com>
2021-11-25 19:14:38 +0300

    add products to cart and cart info

--- bot.py
+++ bot.py
@@ -2,13 +2,14 @@ import os
 
 from dotenv import load_dotenv
 
-from store_requests import create_token, get_all_products
+from store_requests import (add_product_to_cart, create_token,
+                            get_all_products, get_cart)
 
 
 def main():
     load_dotenv()
     token = create_token(os.environ['MOLTIN_CLIENT_ID'])
-    print(get_all_products(token))
+    print(get_cart(token, 123))
 
 
 if __name__ == '__main__':
--- store_requests.py
+++ store_requests.py
@@ -23,3 +23,36 @@ def get_all_products(token):
     response.raise_for_status()
 
     return response.json()
+
+
+def add_product_to_cart(token, cart_id, product_id, quantity=1):
+
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json',
+    }
+
+    data = {
+        "data": {
+            "id": product_id,
+            "type": "cart_item",
+            "quantity": quantity,
+        }
+    }
+
+    response = requests.post(f'https://api.moltin.com/v2/carts/{cart_id}/items', headers=headers, json=data)
+    response.raise_for_status()
+
+    return response.json()
+
+
+def get_cart(token, cart_id):
+
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+
+    response = requests.get(f'https://api.moltin.com/v2/carts/{cart_id}', headers=headers)
+    response.raise_for_status()
+
+    return response.json()
