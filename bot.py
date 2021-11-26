4690b7ca0ce27bef4383a4050a7c29f6df88028c
uzapolsky <uzapolsky@gmail.com>
2021-11-26 14:48:02 +0300

    add echobot with state machine

--- bot.py
+++ bot.py
@@ -1,16 +1,74 @@
 import os
 
+import redis
 from dotenv import load_dotenv
+from telegram.ext import (CallbackQueryHandler, CommandHandler, Filters,
+                          MessageHandler, Updater)
 
 from store_requests import (add_product_to_cart, create_token,
                             get_all_products, get_cart)
 
+_database = None
 
-def main():
-    load_dotenv()
-    token = create_token(os.environ['MOLTIN_CLIENT_ID'])
-    print(get_cart(token, 123))
+def start(update, context):
+
+    update.message.reply_text(text='Привет!')
+    return "ECHO"
+
+
+def echo(update, context):
+
+    users_reply = update.message.text
+    update.message.reply_text(users_reply)
+    return "ECHO"
+
+
+def handle_users_reply(update, context):
+
+    db = get_database_connection()
+    if update.message:
+        user_reply = update.message.text
+        chat_id = update.message.chat_id
+    elif update.callback_query:
+        user_reply = update.callback_query.data
+        chat_id = update.callback_query.message.chat_id
+    else:
+        return
+    if user_reply == '/start':
+        user_state = 'START'
+    else:
+        user_state = db.get(chat_id).decode("utf-8")
+    
+    states_functions = {
+        'START': start,
+        'ECHO': echo
+    }
+    state_handler = states_functions[user_state]
+
+    try:
+        next_state = state_handler(update, context)
+        db.set(chat_id, next_state)
+    except Exception as err:
+        print(err)
+
+
+def get_database_connection():
+
+    global _database
+    if _database is None:
+        database_password = os.getenv("DATABASE_PASSWORD")
+        database_host = os.getenv("DATABASE_HOST")
+        database_port = os.getenv("DATABASE_PORT")
+        _database = redis.Redis(host=database_host, port=database_port, password=database_password)
+    return _database
 
 
 if __name__ == '__main__':
-    main()
+    load_dotenv()
+    token = os.getenv("TG_BOT_TOKEN")
+    updater = Updater(token)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CallbackQueryHandler(handle_users_reply))
+    dispatcher.add_handler(MessageHandler(Filters.text, handle_users_reply))
+    dispatcher.add_handler(CommandHandler('start', handle_users_reply))
+    updater.start_polling()
